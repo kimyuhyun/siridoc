39d579d41840b3b5d64c2749fe4877bbd3ce86dc
kimyuhyun <escnet@naver.com>
2023-04-17 13:51:01 +0900

    2

--- routes/aud.js
+++ routes/aud.js
@@ -1,15 +1,15 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const db = require('../db');
-const aes256util = require('../AES256Util');
-const utils = require('../Utils');
+const db = require("../db");
+const aes256util = require("../AES256Util");
+const utils = require("../Utils");
 
 async function checkToken(req, res, next) {
     console.log(req.query.token);
     if (!req.query.token) {
         res.send({
             code: 0,
-            msg: 'token is empty',
+            msg: "token is empty",
         });
         return;
     }
@@ -19,7 +19,7 @@ async function checkToken(req, res, next) {
     if (!token) {
         res.send({
             code: 0,
-            msg: 'token is invalid',
+            msg: "token is invalid",
         });
         return;
     }
@@ -27,41 +27,39 @@ async function checkToken(req, res, next) {
     if (token < after10s) {
         res.send({
             code: 0,
-            msg: 'token is expired',
+            msg: "token is expired",
             // msg: `${moment(new Date(token)).format('YY-MM-DD HH:mm:ss')} : ${moment(new Date(after10s)).format('YY-MM-DD HH:mm:ss')}`
         });
         return;
     }
     next();
 }
-router.get('/', checkToken, async function(req, res, next) {
-
-    
-    res.send('join');
+router.get("/", checkToken, async function (req, res, next) {
+    res.send("join");
 });
 
-router.post('/write', checkToken, async function(req, res, next) {
+router.post("/write", checkToken, async function (req, res, next) {
     const table = req.body.table;
     const idx = req.body.idx;
 
     delete req.body.idx;
     delete req.body.table;
-    delete req.body.created;
-    delete req.body.modified;
+    // delete req.body.created;
+    // delete req.body.modified;
 
     var sql = ``;
     var records = [];
     records.push(table);
 
     for (key in req.body) {
-        if (req.body[key] != 'null') {
-            if (key == 'pass1') {
+        if (req.body[key] != "null") {
+            if (key == "pass1") {
                 if (req.body[key]) {
-                    sql += key + '= PASSWORD(?), ';
+                    sql += key + "= PASSWORD(?), ";
                     records.push(req.body[key]);
                 }
             } else {
-                sql += key + '= ?, ';
+                sql += key + "= ?, ";
                 records.push(req.body[key]);
             }
         }
@@ -72,25 +70,30 @@ router.post('/write', checkToken, async function(req, res, next) {
         sql = `UPDATE ?? SET ${sql} modified = NOW() WHERE idx = ?`;
         var rs = await utils.queryResult(sql, records);
         rs.code = 1;
-        rs.msg = '수정되었습니다.';
+        rs.msg = "수정되었습니다.";
         res.send(rs);
     } else {
-        sql = `INSERT INTO ?? SET ${sql} created = NOW(), modified = NOW()`;
+        console.log(records);
+        if (req.body.created) {
+            sql = `INSERT INTO ?? SET ${sql.slice(0, -2)}`;
+        } else {
+            sql = `INSERT INTO ?? SET ${sql} created = NOW(), modified = NOW()`;
+        }
         var rs = await utils.queryResult(sql, records);
         rs.code = 1;
-        rs.msg = '등록되었습니다.';
+        rs.msg = "등록되었습니다.";
         res.send(rs);
     }
 });
 
-router.post('/delete', checkToken, async function(req, res, next) {
+router.post("/delete", checkToken, async function (req, res, next) {
     const table = req.body.table;
     const idx = req.body.idx;
     const sql = `DELETE FROM ?? WHERE idx = ?`;
     const params = [table, idx];
     var rs = await utils.queryResult(sql, params);
     rs.code = 1;
-    rs.msg = '삭제되었습니다.';
+    rs.msg = "삭제되었습니다.";
     res.send(rs);
 });
 
