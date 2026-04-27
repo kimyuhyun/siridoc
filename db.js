a6eb33c4d0b2d3ce4feaa8ac6a6ee3f7452cda9a
kimyuhyun <escnet@naver.com>
2026-04-28 07:44:41 +0900

    2

--- db.js
+++ db.js
@@ -1,14 +1,15 @@
-require('dotenv').config();
-let maria = require('mysql2');
+require("dotenv").config();
+const maria = require("mysql2");
 
-let option = {
+const option = {
     host: process.env.DB_HOST,
-    post: process.env.DB_SERVER_PORT,
+    port: process.env.DB_SERVER_PORT,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
-    dateStrings: 'date'
 };
-let conn = maria.createConnection(option);
+
+var conn = maria.createPool(option);
+
 module.exports = conn;
 module.exports.connAccount = option;
